9dc3c568d0133b1ecfa7dff499ffd93954b5246b
潘劭秋 <stephen@panshaoqiudeMBP.gateway>
2019-10-12 15:07:15 +1100

    add read file and finish

---
+++ README.md
@@ -0,0 +1,9 @@
+# binarysearch 找最小值 思路
+
+* Question link  https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/
+* [leetcode_153min.py](./leetcode_153min.py):
+
+举例 数组[3,4,5,1,2] 
+* 先判断向哪边切割， 找出最初的mid，并将这个mid根据情况设置成start，还是end
+* 如果是有序数组，nums[0] 就是最小的
+* 如果是乱序数组， 找到乱序数组中最小数所在的有序数组 比如 [3,4,5,1,2]中找到 [1，2]数组并且返回第一个数字即1对应的序列  
--- leetcode_153min.py
+++ leetcode_153min.py
@@ -1,15 +1,14 @@
 def binary_search(nums, start, end):
-
     mid = int(start+(end-start)/2)
+    # [1,2,3,4,5]   ->
     if nums[end] > nums[start]:
         return start
     if nums[end] < nums[start]:
+        # [3,1,2]  <-—·
         if nums[mid] < nums[start]:
             return binary_search(nums,start,mid)
+        #[5,1,2,3]   ->
         return binary_search(nums,mid+1,end)
-
-    if len(nums) > 1 and mid != end and nums[mid] > nums[mid+1]:
-        return mid+1
     return mid
 class Solution:
     def findMin(self, nums: List[int]) -> int:
