8977c55d3fa1eef63dbf80bb48c4d47d2b5d32ee
stephen <1191367164@qq.com>
2019-10-15 20:07:13 +0800

    first success

--- leetcode_33_Search_in_Rotated_Sorted_Array.py
+++ leetcode_33_Search_in_Rotated_Sorted_Array.py
@@ -7,38 +7,41 @@ def binary_search(nums, start, end, target):
     if mid == start or mid == end:
         if nums[mid] != target:
             return -1
+    if nums[mid] == target:
+        return mid
     if nums[end] > nums[start]:
         if target < nums[mid] and mid > 0:
             return binary_search(nums, start, mid-1, target)
         elif target > nums[mid] and mid < len(nums) - 1:
             return binary_search(nums, mid+1, end, target)
     if nums[end] < nums[start]:
-        if target < nums[end] and target > nums[mid]:
+        if target < nums[end] and nums[mid] > nums[end]:
             return binary_search(nums, mid+1 , end, target)
-        if target < nums[end] and target < nums[mid]:
-            return binary_search(nums, start, mid-1, target)
-        if target > nums[end] and target < nums[mid]:
-            return binary_search(nums, start, mid - 1, target)
-        if target > nums[end] and target > nums[mid]:
-            return binary_search(nums, mid+1, end, target)
+        if target < nums[end] and nums[mid] < nums[end]:
+            if nums[mid] < target:
+                return binary_search(nums, mid+1 , end, target)
+            elif nums[mid] > target:
+                return binary_search(nums, start, mid-1, target)
+        if target > nums[end] and nums[mid] > nums[end]:
+            if nums[mid] < target:
+                return binary_search(nums,mid+1, end, target)
+            elif nums[mid] > target:
+                return binary_search(nums, start, mid - 1, target)
+        if target > nums[end] and nums[mid] < nums[end]:
+            if nums[mid] < target:
+                return binary_search(nums,start, mid-1, target)
+            elif nums[mid] > target:
+                return binary_search(nums,mid+1, end, target)
     return mid
-def search(nums, target):
-    if len(nums) == 0:
-        return -1
-    if len(nums) == 1:
-        if nums[0] == target:
-            return 0
-        return -1
-    start = 0
-    end = len(nums) - 1
-    index = binary_search(nums, start, end, target)
-    if nums[index] != target:
-        return -1
-    return index
-
-# erro test
-nums = [4,5,6,7,0,1]    #not success
-target = 0
-for num in nums:
-    val = search(nums, num)
-    print(str(num) +' index is '+ str(val))
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        if len(nums) == 0:
+            return -1
+        if len(nums) == 1:
+            if nums[0] == target:
+                return 0
+            return -1
+        start = 0
+        end = len(nums) - 1
+        index = binary_search(nums, start, end, target)
+        return index
