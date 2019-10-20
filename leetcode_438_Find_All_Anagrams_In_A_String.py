ec7c2eb41626571b888ca242c62b5db888e819a7
shaoqiuPan <stephenpsq@gmail.com>
2019-10-20 16:32:38 +0800

    second try, use dictionary, still erro because for in for

--- leetcode_438_Find_All_Anagrams_In_A_String.py
+++ leetcode_438_Find_All_Anagrams_In_A_String.py
@@ -1,20 +1,33 @@
-# use normal, erro is overtime
-def findAnagrams(s:str, p: str):
-    if len(s) <= 20100 and len(p) <= 20100:
-        p = sorted(p)
-        arr = []
+def create_dict(str):
+    arr = dict()
+    num = 1
+    for i in str:
+        arr[i] = 0
+    for i in str:
+        if arr[i] != 0:
+            num = arr[i] + 1
+        arr[i] = num
+        num = 1
+    return arr
+
+class Solution:
+    def findAnagrams(self, s: str, p: str) -> List[int]:
+        target = create_dict(p)
+        p_len = len(p)
+        arr_save = []
+        if len(s) < p_len:
+            return arr_save
+        #print("p dict:" + str(target))
         for i in range(len(s)):
-            print ('i is ' + str(i))
-            print('len is ' + str(len(s) - 1))
-            if p == sorted(s[i:len(p)+i]):
-                print('p is ' +str(p))
-                print('s is '+ str(sorted(p[i:len(p)+i])))
-                arr.append(i)
-                print(arr)
-        return arr
-
-
-# s = "aba"
-# p = "a"
-# arr = findAnagrams(s,p)
-# print(arr)
+            count = 0
+            arr = create_dict(s[i:p_len+i])
+           # print("arr dict:" + str(arr))    
+            for j in s[i:p_len+i]:
+                if arr.get(j) == None or target.get(j) == None:
+                    continue
+                if arr[j] == target[j] :
+                    count = count + 1
+                 #   print("count is "+ str(count))
+                if count == p_len:
+                    arr_save.append(i)
+        return arr_save
