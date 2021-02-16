3b35bd895c78b3dcbec1495171f1dbb65563bc53
shao <stephenpsq@gmail.com>
2021-02-16 16:21:06 +0800

    add rick's code

--- Linedlist系列/leetcode_25_ReverseNodes.py
+++ Linedlist系列/leetcode_25_ReverseNodes.py
@@ -5,7 +5,8 @@ class ListNode:
         self.val = val
         self.next = next
 
-class Solution:
+
+class Solution1:
     def reverseKGroup(self, head: ListNode, k: int) -> ListNode:
         
 
@@ -37,7 +38,7 @@ class Solution:
             j = 0
             
             #设置一个动态的node名字，
-            #想法： 每一组先排好序， 不着急链接，设置一个动态的变量名作为链接点，最后通过循环统一链接
+            #想法： 每一组先排好序， 不着急链接，设置一个动态的变量名作为链接点，最后通过循环统一链接·
 
             connect_Node = h2
             while(j < lenEachGroup):
@@ -68,8 +69,6 @@ class Solution:
             h2 = old_next
             pre_group_connetNode = connect_Node
 
-            
-
             i += 1
 
         # 判断，如果不是整数，说明后面有不变的list需要接上， 如果是整数，末尾添加none
@@ -80,6 +79,78 @@ class Solution:
 
         return head
 
+
+class Solution:
+    def reverseKGroup(self, head: ListNode, k: int) -> ListNode:
+
+        if k == 1:
+            return head
+        # 每组实际更改次数，第一个为0
+        lenEachGroup = k-1
+
+        # List length
+        len_list = 0
+        h1 = head
+
+        while h1 != None:
+            len_list += 1
+            h1 = h1.next
+
+        # Group Nums 
+        groupNums = int(len_list/k)
+
+        #old next就是 new head
+        old_next = None
+        # 循环
+        h2 = head
+        i = 0
+        # i = 1
+        while(i < groupNums):
+            
+            # k为1不需要任何变化
+            j = 0
+            
+            #设置一个动态的node名字，
+            #想法： 每一组先排好序， 不着急链接，设置一个动态的变量名作为链接点，最后通过循环统一链接·
+
+            connect_Node = h2
+            while(j < lenEachGroup):
+                
+                # 即将作为next的值
+                new_next = h2
+
+                #仅作用第一次获得old_next值
+                # if(old_next == None):
+                if(j == 0):
+                    old_next = h2.next
+                h2 = old_next
+
+                #控制最后一次old_next存在的位置，当来到最后一次old next 不会再次前进
+                # if(j < lenEachGroup-1):
+                if(h2.next != None):
+                    old_next = h2.next
+                
+                h2.next = new_next
+                j += 1
+
+        # 通过pre_connectNode和connectnode 链接每一组排好序的list， connectnode是每一组的末尾和开头
+            if(i == 0):
+                head = h2
+                pre_group_connetNode = connect_Node
+            if(i>0):
+                pre_group_connetNode.next = h2
+            h2 = old_next
+            pre_group_connetNode = connect_Node
+
+            i += 1
+
+        # 判断，如果不是整数，说明后面有不变的list需要接上， 如果是整数，末尾添加none
+        if (len_list/k > groupNums):
+            connect_Node.next = h2
+        else:
+            connect_Node.next = None
+
+        return head
 
 if __name__ == "__main__":
     
