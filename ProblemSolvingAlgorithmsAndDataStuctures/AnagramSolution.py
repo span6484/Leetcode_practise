643b311f7e7a47dd9de4e1a5e241d7e7fa2a263e
shao <stephenpsq@gmail.com>
2021-02-28 12:12:43 +0800

    add dictionary method for Anagram

--- ProblemSolvingAlgorithmsAndDataStuctures/AnagramSolution.py
+++ ProblemSolvingAlgorithmsAndDataStuctures/AnagramSolution.py
@@ -11,38 +11,62 @@
 # 四种方法，我们将测试最快的方法
 
 # 目标速度： O(n)
-# 思路：计数法
+# 思路：计数法, 字典
 
 
+#Solution 1   
+# def anagramSolution(s1,s2):
+    
+#     isAnagram = True
+    
+#     c1 = [0] * 26
+#     c2 = [0] * 26
+    
+#     # In [2]: ord('a')
+#     # Out[2]: 97
+#     for i in range(len(s1)):
+#         index = ord(s1[i]) - ord('a')
+#         c1[index] += 1
+
+#     for i in range(len(s2)):
+#         index = ord(s2[i]) - ord('a')
+#         c2[index] += 1
+
+
+#     if c1 == c2:
+#         isAnagram = True
+    
+#     else:
+#         isAnagram = False
+    
+#     return isAnagram
+
+
+
+# Solution 2: dictionary
 def anagramSolution(s1,s2):
-    
     isAnagram = True
+    group1 = {}
+    group2 = {}
+    for i in range(ord('a'), ord('z')+1):
+        group1[chr(i)] = 0
+        group2[chr(i)] = 0
     
-    c1 = [0] * 26
-    c2 = [0] * 26
+    for i in s1:
+        group1[i] += 1
+
+    for i in s2:
+        group2[i] += 1
     
-    # In [2]: ord('a')
-    # Out[2]: 97
-    for i in range(len(s1)):
-        index = ord(s1[i]) - ord('a')
-        c1[index] += 1
-
-    for i in range(len(s2)):
-        index = ord(s2[i]) - ord('a')
-        c2[index] += 1
-
-
-    if c1 == c2:
+    if group1 == group2:
         isAnagram = True
-    
     else:
         isAnagram = False
-    
     return isAnagram
 
 
-a = "tcab"
-b = "act"
+a = "ethe"
+b = "the"
 
 c = anagramSolution(a,b)
 if c:
